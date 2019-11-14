6f54348e5935d072c18ceade2ad921a3d150af89
Fahad <fahad011686@gmail.com>
2019-11-14 05:55:52 -0600

    Gmae logic and readme pics.

--- src/App.js
+++ src/App.js
@@ -6,33 +6,59 @@ import cards from "./cards.json";
 
 class Game extends React.Component {
   state = {
-    cardPicked: '',
-    score: 0
+    cardPicked: [],
+    score: 0,
+    strikes: 0
   }
 
   componentDidMount() {
     this.setState({ score: 0 })
-
   }
 
-  handleClick=()=>{
-    alert("You picked a card:" + this.id)
-    this.setState({ cardPicked: this.id })
+  winCheck = () => {
+    if (this.state.cardPicked[0] == this.state.cardPicked[1]) {
+      let newScore = (this.state.score + 1);
+      this.setState({ score: newScore })
+      alert("You got a point!")
+    }
+    else {
+      let newStrikes = (this.state.strikes + 1);
+      this.setState({ strikes: newStrikes })
+      alert("That was incorrect.")
+    }
+  }
+
+  gameCheck = () => {
+    if (this.state.cardPicked.length > 1) {
+      winCheck()
+    }
+  }
+
+  handleClick = () => {
+    alert("You picked a card:" + this.name)
+    this.setState({ cardPicked: this.name })
+    gameCheck();
   }
 
   render() {
     return (
       <div>
-        {
-          cards.map(card => (
-            <MakeCard
-              id={card.id}
-              name={card.name}
-              alt={card.alt}
-              handleClick={this.handleClick}
-            />
-          ))
-        }
+        <div>
+          <p>Score: {this.state.score}</p>
+          <p>Lives Left: {(3 - this.state.strikes)}</p>
+        </div>
+        <div>
+          {
+            cards.map(card => (
+              <MakeCard
+                id={card.id}
+                name={card.name}
+                alt={card.alt}
+                handleClick={this.handleClick}
+              />
+            ))
+          }
+        </div>
       </div>
     )
   }
